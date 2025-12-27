d39fb6743e66640821014beab8655f96c9f04a9f
br0067149533@enel.com <jbmarreiro@ayesa.com>
2025-12-27 17:31:08 -0300

    chore: updade shadcn theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return <Button className="my-1 bg-red-500 p-1">Click me</Button>;
+  return <Button className="p-1">Click me</Button>;
 }
