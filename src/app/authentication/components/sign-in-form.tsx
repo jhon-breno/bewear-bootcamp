a1642e5d3221d7ef2beb1932b0e71d0ad3487c4c
br0067149533@enel.com <jbmarreiro@ayesa.com>
2025-12-28 12:37:45 -0300

    feat: add sign out

--- src/app/authentication/components/sign-in-form.tsx
+++ src/app/authentication/components/sign-in-form.tsx
@@ -86,7 +86,7 @@ const SignInForm = () => {
 
   return (
     <>
-      <Card>
+      <Card className="w-full">
         <CardHeader>
           <CardTitle>Entrar</CardTitle>
           <CardDescription>Faça login para continuar.</CardDescription>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
-import { Button } from "@/components/ui/button";
+import Header from "@/components/common/header";
 
-export default function Home() {
-  return <Button className="p-1">Click me</Button>;
-}
+const Home = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+export default Home;
